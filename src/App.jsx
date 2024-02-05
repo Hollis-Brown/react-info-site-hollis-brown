ba77d5f73d95605893a33f9d2d804f2c138f8504
Hollis <hollis.brown.cohort231@gmail.com>
2024-02-05 10:10:20 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    );
+}
