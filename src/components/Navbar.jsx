dc9b4fd431bf2af1431265635d0b1b890919d232
Hollis <hollis.brown.cohort231@gmail.com>
2024-02-12 13:14:28 -0800

    This is the final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ReactIcon from '../assets/react-icon-small.png';
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="./assets/react-icon-small.png" className="nav--icon" alt="React Icon" />
+            <img src={ReactIcon} className="nav--icon" alt={"ReactIcon"} />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
